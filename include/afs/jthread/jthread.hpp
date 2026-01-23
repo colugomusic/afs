68130bd9de4e396a04dbebde67e77411e5a4e452
Colugo <colugomusic@gmail.com>
2026-01-23 12:57:49 +0000

    Update jthread.hpp

--- include/afs/jthread/jthread.hpp
+++ include/afs/jthread/jthread.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #if defined(__APPLE__)
-#include "jthread-impl.hpp"
+#include "third-party/jthread.hpp"
 namespace std { using jthread = nonstd::jthread; using stop_token = nonstd::stop_token; }
 #else
 #include <thread>
